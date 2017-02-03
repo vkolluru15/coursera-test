5414247e0ab5d6cdb25c95df2c64387a24eefd0b
venkata sateesh kolluru <venkata sateesh kolluru>
2017-02-03 18:33:01 -0500

    angular js mod1 solution

---
+++ Angular/mod1-solution/scripts/app.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('lunchMenu', [])
+.controller('lunchMwnuController', lunchMwnuController)
+
+
+lunchMwnuController.$inject = ['$scope'];
+
+ function lunchMwnuController ($scope) {
+
+  $scope.items = "";
+  $scope.message = "";
+  $scope.checkQuantity = function () {  	
+  	if($scope.items == ""){  		
+  		$scope.message = "";
+  	}else{
+  	var menuItems = $scope.items.trim().split(',');
+  	if(menuItems.length <=3 && menuItems.length >= 1 ){
+  		$scope.message = "Enjoy!";  		
+  	}else{
+  		$scope.message =  "Too much!";  		
+  	}
+  }
+  	
+    
+  };
+};
+
+})();
