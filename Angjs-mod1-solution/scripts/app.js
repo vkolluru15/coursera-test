f6d4a4fe7d5d24f563a7bb840a15092052d0ee1d
venkata sateesh kolluru <venkata sateesh kolluru>
2017-02-03 19:20:32 -0500

    mod5 assignment additional changes

--- Angjs-mod1-solution/scripts/app.js
+++ Angjs-mod1-solution/scripts/app.js
@@ -11,15 +11,19 @@ lunchMwnuController.$inject = ['$scope'];
 
   $scope.items = "";
   $scope.message = "";
+  $scope.messageStyle = "";
   $scope.checkQuantity = function () {  	
   	if($scope.items == ""){  		
   		$scope.message = "Please enter the data first";
+      $scope.messageStyle = "Error";
   	}else{
   	var menuItems = $scope.items.trim().split(',');
   	if(menuItems.length <=3 && menuItems.length >= 1 ){
-  		$scope.message = "Enjoy!";  		
+  		$scope.message = "Enjoy!";  
+      $scope.messageStyle = "Good";		
   	}else{
-  		$scope.message =  "Too much!";  		
+  		$scope.message =  "Too much!";  
+      $scope.messageStyle = "Good";   		
   	}
   }
   	
